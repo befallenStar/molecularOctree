518da4abd44c52b23febc7f43d919b30df3738c4
Junenatte-and-little-pig <sy5622_5@126.com>
2021-07-07 11:56:21 +0800

    Initial commit

---
+++ demo_v1.0.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+import numpy as np
+import open3d as o3d
+
+
+def main():
+    pcd = o3d.io.read_point_cloud("./NH3C2O2H2.ply")
+    print(pcd)
+    # o3d.visualization.draw_geometries([pcd])
+    downpcd = pcd.voxel_down_sample(voxel_size=0.5)
+    print(downpcd)
+    # o3d.visualization.draw_geometries([pcd])
+    octree = o3d.geometry.Octree(max_depth=4)
+    octree.convert_from_point_cloud(pcd)
+    o3d.visualization.draw_geometries([octree])
+
+
+if __name__ == '__main__':
+    main()
